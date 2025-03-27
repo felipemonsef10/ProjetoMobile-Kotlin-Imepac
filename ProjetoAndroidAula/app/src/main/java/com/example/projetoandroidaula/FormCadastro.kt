756658ed1f4418bfde11e76c62f7f39eaff4eca0
unknown <231-000786@IMEPACAD.local>
2025-03-27 20:48:29 -0300

    Quase finalizando a conexão com o firebase

--- ProjetoAndroidAula/app/src/main/java/com/example/projetoandroidaula/FormCadastro.kt
+++ ProjetoAndroidAula/app/src/main/java/com/example/projetoandroidaula/FormCadastro.kt
@@ -1,12 +1,16 @@
 package com.example.projetoandroidaula
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Snackbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class FormCadastro : AppCompatActivity() {
     //Criando as variáveis para receber os id
@@ -34,25 +38,40 @@ class FormCadastro : AppCompatActivity() {
 
             if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
                 val mensagemErro = "Campos não preenchidos, tente novamente."
-
-
-
+                val snackbar = Snackbar.make(it, mensagemErro, Snackbar.LENGTH_LONG)
+                snackbar.show();
+            } else {
+                cadastrarUsuario(it)
             }
-
-
-
-
-
         }
+    }
 
 
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    @SuppressLint("ShowToast")
+    fun cadastrarUsuario(it: View){
+        val email = edit_email.text.toString().trim()
+        val senha = edit_senha.text.toString().trim()
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
+            .addOnCompleteListener { task-> //task é o objeto do resultado vindo do firebase
+                if (task.isSuccessful){
+                    val mensagemOk = "Cadastro realizado com sucesso"
+                    val snackbar = Snackbar.make(it, mensagemOk, Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                } else {
+                    val mensagemErro = "Erro ao cadastrar usuário"
+                    val snackbar = Snackbar.make(it, mensagemErro, Snackbar.LENGTH_LONG);
+                    snackbar.show()
+                }
         }
     }
 }
+
+
+
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
