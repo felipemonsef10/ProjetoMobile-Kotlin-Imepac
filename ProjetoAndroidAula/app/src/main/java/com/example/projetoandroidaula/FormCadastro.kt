1e5ac3a5a86f561eb949007506968d8fd4202069
unknown <231-000786@IMEPACAD.local>
2025-04-01 20:33:57 -0300

    Salvando no fire base os dados do cadastro - OK

--- ProjetoAndroidAula/app/src/main/java/com/example/projetoandroidaula/FormCadastro.kt
+++ ProjetoAndroidAula/app/src/main/java/com/example/projetoandroidaula/FormCadastro.kt
@@ -1,16 +1,17 @@
 package com.example.projetoandroidaula
 
-import android.annotation.SuppressLint
+//import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Snackbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+//import androidx.compose.material3.Snackbar
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FormCadastro : AppCompatActivity() {
     //Criando as variáveis para receber os id
@@ -31,47 +32,64 @@ class FormCadastro : AppCompatActivity() {
         edit_senha = findViewById(R.id.edit_senha)
         btnCadastrar = findViewById(R.id.btn_cadastro)
 
-        btnCadastrar.setOnClickListener {
+        btnCadastrar.setOnClickListener { it
             val nome = edit_nome.text.toString().trim()
             val email = edit_email.text.toString().trim()
             val senha = edit_senha.text.toString().trim()
 
             if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
                 val mensagemErro = "Campos não preenchidos, tente novamente."
-                val snackbar = Snackbar.make(it, mensagemErro, Snackbar.LENGTH_LONG)
+                val snackbar = Snackbar.make(it, mensagemErro, Snackbar.LENGTH_LONG);
                 snackbar.show();
             } else {
-                cadastrarUsuario(it)
+                cadastrarUsuario(it);
             }
         }
     }
 
 
-    @SuppressLint("ShowToast")
+//    @SuppressLint("ShowToast")
     fun cadastrarUsuario(it: View){
         val email = edit_email.text.toString().trim()
         val senha = edit_senha.text.toString().trim()
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
             .addOnCompleteListener { task-> //task é o objeto do resultado vindo do firebase
                 if (task.isSuccessful){
+                    salvarDadosUsuario()
                     val mensagemOk = "Cadastro realizado com sucesso"
                     val snackbar = Snackbar.make(it, mensagemOk, Snackbar.LENGTH_LONG);
                     snackbar.show();
                 } else {
                     val mensagemErro = "Erro ao cadastrar usuário"
                     val snackbar = Snackbar.make(it, mensagemErro, Snackbar.LENGTH_LONG);
-                    snackbar.show()
+                    snackbar.show();
                 }
         }
     }
+
+
+    fun salvarDadosUsuario() {
+        val db = FirebaseFirestore.getInstance()
+        val nome = edit_nome.text.toString().trim()
+        val usuarioID = FirebaseAuth.getInstance().currentUser?.uid
+        val email = FirebaseAuth.getInstance().currentUser?.email
+
+        if (usuarioID != null && email != null) {
+            val usuarios = hashMapOf(
+                "nome" to nome,
+                "email" to email,
+                "uid" to usuarioID
+            )
+            db.collection("Usuarios")
+                .add(usuarios)
+                .addOnSuccessListener { documentReference ->
+                    println("Documento adicionado com ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    println("Erro ao adicionar documento: ${e}")
+                }
+        } else {
+            println("Erro na autenticação")
+        }
+    }
 }
-
-
-
-
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
